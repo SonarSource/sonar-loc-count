6ebeff415af454c121f097052b37b16a0bb15d63
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2022-12-22 22:21:17 +0100

    Add files via upload

---
+++ gitlab2_com.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#set -x
+#******************************************************************#
+#                                                                  #
+#  @project     : LoC Counting bash Scripts                        #
+#  @package     :                                                  #
+#  @subpackage  : gitlab2                                          #
+#  @access      :                                                  #
+#  @paramtype   : connectionToken,groupName                        #
+#  @argument    :                                                  #
+#  @description : Get Number ligne of Code in GitLab DevOPS        #
+#  @usage : ./gitlab2_com.sh <token> <groupName>                   #                                                              
+#                                                                  #
+#                                                                  #
+#  @author Emmanuel COLUSSI                                        #
+#  @version 1.01                                                   #
+#                                                                  #
+#******************************************************************#
+
+if [ $# -lt 2 ]; then
+    echo "Usage: `basename $0` <token> <groupName>"
+    exit
+fi
+
+ # Set Variables token, groupName, BaseAPI
+connectionToken=$1
+groupname=$2
+BaseAPI="https://gitlab.com/api/v4"
+
+LISTF=""
+LIST=""
+i=1
+SED="gsed"
+
+# Get List of Repositories - List 
+curl -s -u $connectionToken: $BaseAPI/groups/$groupname | jq -r '.projects[] | "\(.name):\(.id)"' | while IFS=: read -r Name ID;
+do
+  echo "-----------------------------------------------------------------"
+  printf "Repository Number :$i  Name : $Name id : $ID\n"
+# Get List of Branches
+   let "i=i+1"
+   NameFile=` echo $Name|$SED s/' '/'-'/g`
+    
+    curl -s -u $connectionToken: $BaseAPI/projects/$ID/repository/branches | jq -r '.[].name' | while read -r BrancheName ;
+    do
+        LISTF="${NameFile}_${BrancheName}.cloc"
+        printf '%s\n' "       Branche Name : $BrancheName"
+        # Create Commad Git clone 
+        git clone https://oauth2:${connectionToken}@gitlab.com/$groupname/${NameFile}.git --depth 1 --branch $BrancheName $NameFile
+        
+        # Run Analyse : run cloc on the local repository
+       cloc $NameFile --force-lang-def=sonar-lang-defs.txt --report-file=${LISTF} --timeout 0
+    
+       # Delete Directory projet
+        /bin/rm -r $NameFile
+       
+        gsed -i "1i\Report for project ${Name} / ${BrancheName}\n" $LISTF
+
+    done   
+    # Combine reports
+    echo "Building final report for projet $NameFile : $NameFile.txt "
+    #for j in `ls -la *.cloc|awk '{print $9}'`; do cat $j >>  $NameFile.txt; done
+     echo "-----------------------------------------------------------------"
+  
+    ([ `printf *.cloc` != '*.cloc' ] || [ -f '*.txt' ]) &&  for j in `ls -la *.cloc|awk '{print $9}'`; do cat $j >>  $NameFile.txt; done &&  /bin/rm  *.cloc || echo ""
+
+
+    #/bin/rm  *.cloc
+    LISTF=""
+
+done
+
