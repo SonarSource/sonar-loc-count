f3c90c4e34f08644b7299639c09885dc5cdc547f
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2023-01-05 14:12:11 +0100

    Add files via upload

--- gitlab_com.sh
+++ gitlab_com.sh
@@ -1,84 +1,148 @@
 #!/bin/bash
-# Count LoC for a GitLab group
+#set -x
+#**************************************************************************#
+#                                                                          #
+#  @project     : LoC Counting bash Scripts                                #
+#  @package     :                                                          #
+#  @subpackage  : gitlab3                                                  #
+#  @access      :                                                          #
+#  @paramtype   : connectionToken,groupName or path_with_namespace         #
+#  @argument    :                                                          #
+#  @description : Get Number ligne of Code in GitLab DevOPS                #
+#  @usage : ./gitlab2_com.sh <token> <groupName>                           #                                                              
+#                                                                          #
+#                                                                          #
+#  @author Emmanuel COLUSSI                                                #
+#  @version 1.01                                                           #
+#                                                                          #
+#**************************************************************************#
+
 
 if [ $# -lt 2 ]; then
-    echo "Usage: `basename $0` <token> <groupID>"
+    echo "Usage: `basename $0` <token> <groupName> <root>"
     exit
 fi
 
-apiBase=https://gitlab.com/api/v4
+# Set Variables token, groupName, Namespace, BaseAPI
+# Namespace values : <0> = Browse the groupName - <1> = Browse SubgroupName or Project
+#--------------------------------------------------------------------------------------#
+connectionToken=$1
+groupname=$2
 
-token=$1
-groupID=$2
-reposJson=""
-groupName=$(curl -X GET "PRIVATE-TOKEN: $1" "https://gitlab.com/api/v4/groups/$2" | jq '.path' --raw-output)
-
-echo $groupName
-URL="$apiBase/groups/$groupID/projects?per_page=100"
-while [ "$URL" ]; do
-  echo "Info: checking URL:$URL"
-  RESP=$(curl -i -s -f -u "$user:$token" "$URL")
-  if [ $? -ne 0 ]; then
-    echo "######################"
-    echo "Error getting recursive repo information from GitHub with URL:$URL"
-    echo "running the command with verbose output enabled:"
-    echo "######################"
-    curl -i -v -u "$user:$token" "$URL"
-    exit 2
-  fi
-  HEADERS=$(echo "$RESP" | sed '/^\r$/q')
-  URL=$(echo "$HEADERS" | sed -n -E 's/link:.*<(.*?)>; rel="next".*/\1/p')
-  if [ $? -ne 0 ]; then
-    echo "######################"
-    echo "Error with extracting next page link from headers:$HEADERS"
-    echo "MacOS users need to switch to a standard sed implementation, e.g., gnu-sed"
-    echo "######################"
-    exit 3
-  fi
-  reposJson="$reposJson $(echo "$RESP" | sed '1,/^\r$/d')"
-done
-
-readarray -t repos < <(jq -c '.[] | {name: .name, full_name: .full_name, default_branch: .default_branch}' <<< $reposJson)
-if [ $? -ne 0 ]; then
-    echo "######################"
-    echo "readarray was added to bash in version 4"
-    echo "Make sure this script runs with such bash"
-    echo "MacOS users might need to update their bash, and the first line in this script to point at the updated bash"
-    echo "######################"
-    exit 4
+if [[ "$2" =~ .*"/".* ]]; then 
+       Namespace=1
+    else 
+       Namespace=0
 fi
-# Retrieve list of repos in the group
-projectsJson=$(curl -f -s $apiBase/groups/$groupID/projects?private_token=$token)
+
+BaseAPI="https://gitlab.com/api/v4"
+#--------------------------------------------------------------------------------------#
+
+# If you use mac osx you have to install the gnu-sed (brew install gnu-sed) then you set in the scripts the variable SED. SED=gsed
+# For linux SED=sed
+SED="gsed"
+i=1
+LISTF=""
+LIST=""
+NBCLOC="cpt.txt"
+cpt=0
 
 
-#readarray -t repos < <(jq -c '.[] | {name: .name, default_branch: .default_branch}' <<< $projectsJson)
+# Test if request for for 1 Project or more Project in GroupName
+if [ $Namespace -eq 1 ]; then
+        groupname1=` echo $groupname|$SED s/'\/'/'%2f'/g`
+        GetAPI="/projects/$groupname1"
+        jq_args="\"\(.name):\(.id):\(.http_url_to_repo)\""
 
-echo "Counting code from repos:"
-for repo in "${repos[@]}"; do
-    name=$(jq -r '.name' <<< $repo)
-    branch=$(jq -r '.default_branch' <<< $repo)
-    echo $name  - $branch    
-done
+    else  GetAPI="/groups/$groupname/projects?include_subgroups=true"
+          jq_args=".[] | \"\(.name):\(.id):\(.http_url_to_repo)\""
+fi
 
-# Checkout each repo and count code
-fileList=""
 
-for repo in "${repos[@]}"; do
-    name=$(jq -r '.name' <<< $repo)
-    branch=$(jq -r '.default_branch' <<< $repo)
-    remoteUrl=https://oauth2:$token@gitlab.com/$groupName/$name.git
-    fileList+="$name.cloc "
-    echo Checking out $name - $branch
-    git clone $remoteUrl --depth 1 --branch $branch $name
-    echo Counting $name - $branch
-    cloc $name --force-lang-def=sonar-lang-defs.txt --report-file=$name.cloc  
-    rm -rf $name
-done
+# Get List of Repositories : get Name , ID and http_url_to_repo
+curl -s -u $connectionToken: $BaseAPI$GetAPI|jq -r ''"${jq_args}"''| while IFS=: read -r Name ID Repourl;
 
-# Combine reports
-echo "Building final report:"
+do
+  echo "-----------------------------------------------------------------"
+  echo -e "Repository Number :$i  Name : $Name id : $ID"
+# Get List of Branches
+   #let "i=i+1"
+   NameFile=` echo $Name|$SED s/' '/'-'/g`
 
-cloc --sum-reports --force-lang-def=sonar-lang-defs.txt --report-file=$groupName $fileList
-rm *.cloc
+   curl -s -u $connectionToken: $BaseAPI/projects/$ID/repository/branches | jq -r '.[].name' | while read -r BrancheName ;
+    do
+        LISTF="${NameFile}_${BrancheName}.cloc"
+        echo -e "\n       Branche Name : $BrancheName\n"
 
-exit 0;
+        # Format Clone URL : cut <https://> string
+        CLONE="${Repourl:8}"
+
+        # Create Commad Git clone 
+        git clone https://oauth2:${connectionToken}@$CLONE --depth 1 --branch $BrancheName $NameFile
+
+        # Run Analyse : run cloc on the local repository
+        cloc $NameFile --force-lang-def=sonar-lang-defs.txt --report-file=${LISTF} --timeout 0
+    
+       # Delete Directory projet
+        /bin/rm -r $NameFile
+       
+        $SED -i "1i\Report for project ${Name} / ${BrancheName}\n" $LISTF
+
+    done   
+
+# Generate reports
+
+    echo -e "\nBuilding final report for projet $NameFile : $NameFile.txt"
+    echo "-----------------------------------------------------------------------------------------------------------------------" 
+
+    # BRTAB2 : array with branche Name , The index is number max of cloc 
+    # NBTAB1 : array with number max of cloc by branche
+  
+    ([ `printf *.cloc` != '*.cloc' ] || [ -f '*.txt' ]) &&  for j in `ls -la *.cloc|awk '{print $9}'`; do  CMD1=`cat $j |grep SUM:|awk '{print $5}'`;BRTAB2["$CMD1"]=${j%?????};NBTAB1+="$CMD1 ";cat $j >>  $NameFile.txt;done && /bin/rm *.cloc || echo ""
+
+    # Find 
+    IFS=' ' NBRCLOC=( $NBTAB1 )
+    IFS=$'\n' sorted=( $(sort -nr <<<"${NBRCLOC[*]}") )
+    INDEX01=${sorted[0]}
+
+    if [ -z "$INDEX01" ]; then 
+                MESSAGE01="0"
+                MESSAGE02="No"
+                INDEX01=0  
+
+        else 
+            MESSAGE01="$INDEX01"
+            SEA="${NameFile}_"
+            MESSAGE02=`echo ${BRTAB2[${INDEX01}]}| $SED s/$SEA/''/g`
+    fi      
+
+    echo -e "The maximum number of lines of code in the < $NameFile > project is : < ${MESSAGE01} > for the branch : < ${MESSAGE02} > \n"
+    echo -e "\n---------------------------------------------------------------------------------------------------------------------" >> $NameFile.txt
+    echo -e "\nThe maximum number of lines of code in the < $NameFile > project is : < ${MESSAGE01} > for the branch : < ${MESSAGE02} > \n" >> $NameFile.txt
+    echo -e "-----------------------------------------------------------------------------------------------------------------------" >> $NameFile.txt 
+
+    # set Nbr Loc by Project in File cpt.txt
+    echo "${INDEX01}" >> $NBCLOC
+    
+    LISTF=""
+    NBTAB1=()
+    BRTAB2=()
+    let "i=i+1"
+    
+done 
+
+# Generate Gobal report
+while read line  
+do   
+   cpt=$(expr $cpt + $line)
+done < $NBCLOC
+
+/bin/rm $NBCLOC
+
+echo -e "\n-------------------------------------------------------------------------------------------"
+echo -e "The maximum number of lines of code for all projects is : < ${cpt} > result in <global.txt>\n"
+echo -e "\n-------------------------------------------------------------------------------------------"
+
+echo -e "-------------------------------------------------------------------------------------------\n" > global.txt
+echo -e "The maximum number of lines of code for all projects is : < ${cpt} > result in <global.txt>\n" >> global.txt
+echo -e "---------------------------------------------------------------------------------------------" >> global.txt
