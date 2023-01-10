f90a5482ae039c8e9c8566af15d7761da72ae96a
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2023-01-10 17:38:02 +0100

    Add files via upload

--- bitbucket_org.sh
+++ bitbucket_org.sh
@@ -1,46 +1,146 @@
 #!/bin/bash
-# Count LoC for Bitbucket workspace
+#set -x
+#***********************************************************************************#
+#                                                                                   #
+#  @project     : LoC Counting bash Scripts                                         #
+#  @package     :                                                                   #
+#  @subpackage  : bitbucket                                                         #
+#  @access      :                                                                   #
+#  @paramtype   : user token,workspace ,reponame                                    #
+#  @argument    :                                                                   #
+#  @description : Get Number ligne of Code in Bitbucket DevOPS                      #
+#  @usage : ./bitbucket_org.sh <user> <token> <workspace> and optional <repoName>   #                                                              
+#                                                                                   #
+#                                                                                   #
+#  @author Emmanuel COLUSSI                                                         #
+#  @version 1.01                                                                    #
+#                                                                                   #
+#***********************************************************************************#
+
 
 if [ $# -lt 3 ]; then
-    echo "Usage: `basename $0` <user> <token> <workspace>"
+    echo "Usage: `basename $0` <user> <token> <workspace> and optional <repoName>"
     exit
 fi
 
-apiBase=https://api.bitbucket.org/2.0
+# Set Variables user,token,org,BaseAPI
+#--------------------------------------------------------------------------------------#
 user=$1
-token=$2
-workspace=$3
+connectionToken=$2
+wks=$3
 
-reposJson=$(curl -f -s -u "$user:$token" $apiBase/repositories/$workspace)
+BaseAPI="https://api.bitbucket.org/2.0"
+BaseAPI1="bitbucket.org"
+#--------------------------------------------------------------------------------------#
 
-readarray -t repos < <(jq -c '.values[] | {name: .name, full_name: .full_name, default_branch: .mainbranch.name}' <<< $reposJson)
+# If you use mac osx you have to install the gnu-sed (brew install gnu-sed) then you set in the scripts the variable SED. SED=gsed
+# For linux SED=sed
+SED="gsed"
+i=1
+LISTF=""
+LIST=""
+NBCLOC="cpt.txt"
+cpt=0
 
-echo "Counting code from repos:"
-for repo in "${repos[@]}"; do
-    name=$(jq -r '.full_name' <<< $repo)
-    branch=$(jq -r '.default_branch' <<< $repo)
-    echo $name  - $branch    
-done
+# Test if request for for 1 Repo or more Repo
 
-fileList=""
+if [ -z ${4} ]; then 
+     jq_args=".values[] | \"\(.name):\(.uuid)\""
+     GetAPI="repositories/$wks"
+else 
+    jq_args="\"\(.name):\(.uuid)\"" 
+    GetAPI="repositories/$wks/$4"
+fi
 
-for repo in "${repos[@]}"; do
-    full_name=$(jq -r '.full_name' <<< $repo)
-    name=$(jq -r '.name' <<< $repo)
-    branch=$(jq -r '.default_branch' <<< $repo)
-    remoteUrl=https://$user:$token@bitbucket.org/$full_name.git
-    fileList+="$name.cloc "
-    echo Checking out $full_name - $branch
-    git clone $remoteUrl --depth 1 --branch $branch $name
-    echo Counting $full_name - $branch
-    cloc $name --force-lang-def=sonar-lang-defs.txt --report-file=$name.cloc  
-    rm -rf $name
-done
 
-echo "Building final report:"
+# Get List of Repositories : get Name , ID and http_url_to_repo
+curl -s -u $user:$connectionToken $BaseAPI/$GetAPI|jq -r ''"${jq_args}"''| while IFS=: read -r Name UUID;
 
-cloc --sum-reports --force-lang-def=sonar-lang-defs.txt --report-file=$workspace $fileList
-rm *.cloc
+do
+    echo "-----------------------------------------------------------------------------------------------------------------------" 
+    echo -e "Repository Number :$i  Name : $Name uuid : $UUID"
+    # Get List of Branches
 
-exit 0;
+    # Replace space by - in Repository name for created local file
+    NameFile=` echo $Name|$SED s/' '/'-'/g`
+    # Replace space by %20 in Repository name for Repository URL
+    name1=` echo $Name|$SED s/' '/'%20'/g`
 
+    curl -s -u $user:$connectionToken $BaseAPI/repositories/$wks/$Name/refs/branches | jq -r '.values[].name' | while read -r BrancheName ;
+    do
+        # Get Branche Name without path reference
+        BrancheNameF1=` echo $BrancheName|$SED s/'refs\/heads\/'/''/g`
+        # Replace / by - in Branche Name for created local file
+        BrancheNameF=` echo $BrancheNameF1|$SED s/'\/'/'-'/g|$SED s/' '/'-'/g`
+
+        LISTF="${NameFile}_${BrancheNameF}.cloc"
+        echo -e "\n       Branche Name : $BrancheNameF1\n"
+
+        # Create Commad Git clone 
+        git clone  https://oauth2:${connectionToken}@$BaseAPI1/$wks/$name1 --depth 1 --branch $BrancheNameF1 $NameFile
+
+        # Run Analyse : run cloc on the local repository
+        cloc $NameFile --force-lang-def=sonar-lang-defs.txt --report-file=${LISTF} --timeout 0
+    
+        # Delete Directory projet
+        /bin/rm -r $NameFile
+       
+        $SED -i "1i\Report for project ${Name} / ${BrancheName}\n" $LISTF
+
+    done
+     # Generate reports
+
+        echo -e "\nBuilding final report for projet $NameFile : $NameFile.txt"
+        echo "-----------------------------------------------------------------------------------------------------------------------" 
+
+        # BRTAB2 : array with branche Name , The index is number max of cloc 
+        # NBTAB1 : array with number max of cloc by branche
+  
+        ([ `printf *.cloc` != '*.cloc' ] || [ -f '*.txt' ]) &&  for j in `ls -la *.cloc|awk '{print $9}'`; do  CMD1=`cat $j |grep SUM:|awk '{print $5}'`;BRTAB2["$CMD1"]=${j%?????};NBTAB1+="$CMD1 ";cat $j >>  $NameFile.txt;done && /bin/rm *.cloc || echo ""
+
+        # Find 
+        IFS=' ' NBRCLOC=( $NBTAB1 )
+        IFS=$'\n' sorted=( $(sort -nr <<<"${NBRCLOC[*]}") )
+        INDEX01=${sorted[0]}
+
+        if [ -z "$INDEX01" ]; then 
+                MESSAGE01="0"
+                MESSAGE02="No"
+                INDEX01=0  
+
+            else 
+                MESSAGE01="$INDEX01"
+                SEA="${NameFile}_"
+                MESSAGE02=`echo ${BRTAB2[${INDEX01}]}| $SED s/$SEA/''/g`
+        fi      
+
+        printf "The maximum lines of code in the < %s > project is : < %' .f > for the branch : < %s >\n" "${NameFile}" "${MESSAGE01}" "${MESSAGE02}"
+        echo -e "\n---------------------------------------------------------------------------------------------------------------------" >> $NameFile.txt
+        printf "\nThe maximum lines of code in the < %s > project is : < %' .f > for the branch : < %s >\n" "${NameFile}" "${MESSAGE01}" "${MESSAGE02}" >> $NameFile.txt
+        echo -e "-----------------------------------------------------------------------------------------------------------------------" >> $NameFile.txt 
+
+        # set Nbr Loc by Project in File cpt.txt
+        echo "${INDEX01}" >> $NBCLOC
+    
+        LISTF=""
+        NBTAB1=()
+        BRTAB2=()
+        let "i=i+1"
+    
+done 
+
+# Generate Gobal report
+while read line  
+do   
+   cpt=$(expr $cpt + $line)
+done < $NBCLOC
+
+/bin/rm $NBCLOC
+
+echo -e "\n-------------------------------------------------------------------------------------------"
+printf "The maximum lines of code on the repository is : < %' .f > result in <global.txt>\n" "${cpt}"
+echo -e "\n-------------------------------------------------------------------------------------------"
+
+echo -e "-------------------------------------------------------------------------------------------\n" > global.txt
+printf "The maximum lines of code on the repository is : < %' .f > result in <global.txt>\n" "${cpt}" >> global.txt
+echo -e "---------------------------------------------------------------------------------------------" >> global.txt   
