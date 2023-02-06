acff7b2d6550d269651543d61f4eafa06f3e999c
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2023-02-07 00:16:31 +0100

    Add files via upload
    Update 02

--- PowerShell/azuredevops.ps1
+++ PowerShell/azuredevops.ps1
@@ -11,7 +11,7 @@
 #                                                                                          #
 #                                                                                          #
 #  @author Emmanuel COLUSSI                                                                #
-#  @version 1.01                                                                           #
+#  @version 1.02                                                                           #
 #                                                                                          #
 #******************************************************************************************#
 
@@ -36,104 +36,141 @@ else {
     $organization=$args[1]
     $CLOCPATH=$args[2]
 
-    # Test if request for for 1 Repo or more Repo
+    # Test if request for for 1 Project or more Project 
     if ($args.Length -eq 4) {
       $Project=$args[3]
-      $GetAPI="$organization/$Project/_apis/git/repositories/$Project"
+      $GetAPI="${organization}/_apis/projects/${Project}?api-version=7.0"
     } else {
-      $GetAPI="$organization/_apis/git/repositories?api-version=7.0"
+         $GetAPI="${organization}/_apis/projects?api-version=7.0"
     }
 
     if(Test-Path $CLOCPATH) {
 
       # Encode Authentification Token
       $base64AuthInfo= [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($connectionToken)"))
-      # Set API URL to Get Repositories
+      # Set API URL to Get Project
       $ProjectUrl = "${BaseAPI}/${GetAPI}" 
-      # Get List of Repositories
-      $Repo = (Invoke-RestMethod -Uri $ProjectUrl -Method Get -UseDefaultCredential -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
+      # Get List of Project
+      $Projects = (Invoke-RestMethod -Uri $ProjectUrl -Method Get -UseDefaultCredential -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
       # Get Number of Repositories
-      if ($args.Length -eq 4) { $NumberRepositories=$Repo.count }
-      else { $NumberRepositories=$Repo.value.count }
+      if ($args.Length -eq 4) { $NumberProjects= $Projects.count }
+      else { $NumberProjects= $Projects.value.count }
 
-      Write-Host "`n Number of Repositories : ${NumberRepositories} `n"
+      Write-Host "`n Number of Project : ${NumberProjects} `n"
 
-      # Parse Repositories
+      # Parse Project : Get Repositories
       #--------------------------------------------------------------------------------------#
 
-      for ($j=0; $j -lt $NumberRepositories;$j++) {
+      for ($j=0; $j -lt $NumberProjects;$j++) {
        
-        # Get Repositorie Name and ID
+        # Get Project Name and ID
         if ($args.Length -eq 4) { 
-          $RepoName= $Repo.name
-          $IDrepo=$Repo.id
+          $ProjectName= $Projects.name
+          $IDProject=$Projects.id
         }
         else {
-          $RepoName= $Repo.value[$j].name
-          $IDrepo=$Repo.value[$j].id
+          $ProjectName= $Projects.value[$j].name
+          $IDProject=$Projects.value[$j].id
         }
-        Write-Host "-----------------------------------------------------------------"
-        Write-Host "`n Repository Number :$j  Name : $RepoName id : $IDrepo`n"
+        Write-Host "--------------------------------------------------------------------------------------"
+        Write-Host "`n Peoject Number :$j  Name : $ProjectName id : $IDProject`n"
      
-        # Set API URL to Get Branches
-        $ProjetBranchUrl1="${BaseAPI}/${organization}/${RepoName}/_apis/git/repositories/${IDrepo}/refs?filter=heads/&api-version=7.0"
+        # Set API URL to Get Repo
+    
+        $ProjetRepoUrl1="${BaseAPI}/${organization}/${ProjectName}/_apis/git/repositories?api-version=7.0"
        # [uri]::EscapeDataString( $ProjetBranchUrl)
-        $ProjetBranchUrl= $ProjetBranchUrl1.replace(" ","%20")
-       
-        # Get List of Branches
+        $ProjetRepoUrl= $ProjetRepoUrl1.replace(" ","%20")
+
+        # Get List of Repositories
         try {
-         $Branch = (Invoke-RestMethod -Uri $ProjetBranchUrl -Method Get -UseDefaultCredential -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
+         $Repo = (Invoke-RestMethod -Uri $ProjetRepoUrl -Method Get -UseDefaultCredential -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
         } Catch {
             if($_.ErrorDetails.Message) {
               # Write-Host $_.ErrorDetails.Message
             } else {
-              # Get Number of Branches
-               $NumberBranch=$Branch.value.count
+              # Get Number of Repo
+               $NumberRepo=$Repo.value.count
             }
          }
-        $NumberBranch=$Branch.value.count
+        $NumberRepo=$Repo.value.count
      
-        # Parse Repositories/Branches 
+        # Parse Repositories
         #--------------------------------------------------------------------------------------#
 
-        for ($i = 0; $i -lt $NumberBranch; $i++) {
-          # Get Branche Name 
-          $BranchePathName=$Branch.value[$i].name.replace('refs/heads/','')
-          $BranchList= $Branch.value[$i].name.Split("/")
-          $BrancheName=$BranchList[$BranchList.count -1 ]
+        for ($i = 0; $i -lt $NumberRepo; $i++) {
+
+             # Get Repositorie Name and ID
+            if ($args.Length -eq 4) { 
+                 $RepoName= $Repo.value[$i].name
+                 $IDrepo=$Repo.value[$i].id
+             }
+                else {
+                        $RepoName= $Repo.value[$i].name
+                        $IDrepo=$Repo.value[$i].id
+                }
+          Write-Host "--------------------------------------------------------------------------------------"
+          Write-Host "`n Repository Number :$i  Name : $RepoName id : $IDrepo`n"
+       
+          # Set API URL to Get Branches
+          $ProjetBranchUrl1="${BaseAPI}/${organization}/${RepoName}/_apis/git/repositories/${IDrepo}/refs?filter=heads/&api-version=7.0"
+         # [uri]::EscapeDataString( $ProjetBranchUrl)
+          $ProjetBranchUrl= $ProjetBranchUrl1.replace(" ","%20")
+  
+          # Get List of Branches
+          try {
+           $Branch = (Invoke-RestMethod -Uri $ProjetBranchUrl -Method Get -UseDefaultCredential -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
+          } Catch {
+              if($_.ErrorDetails.Message) {
+                # Write-Host $_.ErrorDetails.Message
+              } else {
+                # Get Number of Branches
+                 $NumberBranch=$Branch.value.count
+              }
+           }
+          $NumberBranch=$Branch.value.count
+       
+          # Parse Repositories/Branches 
+          #--------------------------------------------------------------------------------------#
+          
+          for ($i2 = 0; $i2 -lt $NumberBranch; $i2++) { 
+
+             # Get Branche Name 
+            $BranchePathName=$Branch.value[$i2].name.replace('refs/heads/','')
+            $BranchList= $Branch.value[$i2].name.Split("/")
+            $BrancheName=$BranchList[$BranchList.count -1 ]
          
         
-          # Clone Repository locally
-          Write-Host "`n      Branche Name : ${RepoName}/${BrancheName} `n"
-          $remoteUrl="https://${connectionToken}@dev.azure.com/${organization}/${RepoName}/_git/${RepoName}"
-          # Create Commad Git clone and replace space by %20
-          $RepoName2=$RepoName.replace(" ","_").replace("/","_") 
+            # Clone Repository locally
+            Write-Host "`n      Branche Name : ${RepoName}/${BrancheName} `n"
+            $remoteUrl="https://${connectionToken}@dev.azure.com/${organization}/${ProjectName}/_git/${RepoName}"
+            # Create Commad Git clone and replace space by %20
+            $RepoName2=$RepoName.replace(" ","_").replace("/","_") 
 
-          if (Test-Path -Path $RepoName2) {
-             Remove-Item $RepoName2 -Recurse -Force
-          } else {}
-          $cmdline0=" git clone '" + $remoteUrl.replace(" ","%20") + "' --depth 1 --branch '" + $BranchePathName + "' " + $RepoName2
-          Invoke-Expression -Command $cmdline0  
+            if (Test-Path -Path $RepoName2) {
+                Remove-Item $RepoName2 -Recurse -Force
+            } else {}
+            $cmdline0=" git clone '" + $remoteUrl.replace(" ","%20") + "' --depth 1 --branch '" + $BranchePathName + "' " + $RepoName2
+            Invoke-Expression -Command $cmdline0  
 
-          # Run Analyse : run cloc on the local repository
-          Write-Host "Analyse Counting ${RepoName}/${BrancheName}"
-          $cmdparms2="${RepoName2} --force-lang-def=sonar-lang-defs.txt --ignore-case-ext --report-file=${RepoName2}_${BrancheName}.cloc  --timeout 0"
-          $cmdline2=$CLOCPATH + " " + $cmdparms2
-          Invoke-Expression -Command $cmdline2
+            # Run Analyse : run cloc on the local repository
+            Write-Host "Analyse Counting ${RepoName}/${BrancheName}"
+            $cmdparms2="${RepoName2} --force-lang-def=sonar-lang-defs.txt --ignore-case-ext --report-file=${RepoName2}_${BrancheName}.cloc  --timeout 0"
+            $cmdline2=$CLOCPATH + " " + $cmdparms2
+            Invoke-Expression -Command $cmdline2
 
-          If ( -not (Test-Path -Path ${RepoName2}_${BrancheName}.cloc) )  {
-            "0 Files Analyse in ${RepoName2}/${BrancheName}" | Out-File ${RepoName2}_${BrancheName}.cloc
-          }
+            If ( -not (Test-Path -Path ${RepoName2}_${BrancheName}.cloc) )  {
+             "0 Files Analyse in ${RepoName2}/${BrancheName}" | Out-File ${RepoName2}_${BrancheName}.cloc
+            }
          
-       
-          # Generate report
-          "Result Analyse Counting ${RepoName2} / ${BrancheName}" | Out-File -Append "${RepoName2}.txt"
-          Get-content ${RepoName2}_${BrancheName}.cloc | Out-File -Append "${RepoName2}.txt"
-       
-        }
+            # Generate report
+            "Result Analyse Counting ${RepoName2} / ${BrancheName}" | Out-File -Append "${RepoName2}.txt"
+            Get-content ${RepoName2}_${BrancheName}.cloc | Out-File -Append "${RepoName2}.txt"
+        }   
+        
+      }
          #--------------------------------------------------------------------------------------#
 
-        Write-Host "`nBuilding final report for projet $RepoName : $RepoName.txt"
+        Write-Host "`nBuilding final report for projet $ProjectName : $ProjectName.txt"
 
 
         Get-ChildItem -Path .\* -Include *.cloc |ForEach-Object { $NMCLOCB=Get-content $_.Name |Select-String "SUM:";$NMCLOCB-replace "\s{2,}" , " "| ForEach-Object{$NMCLOCB1=$_.ToString().split(" ");$CLOCBr+=@([PSCustomObject]@{ CLOC=$NMCLOCB1[4] ; BRANCH=${BrancheName}})};Remove-Item $_.Name -Recurse -Force} 
@@ -162,6 +199,7 @@ else {
 
         $clocmax | Out-File -Append "$NBCLOC"
       }  
+    
        #--------------------------------------------------------------------------------------#
 
       # Generate Gobal report
