efde0f2289b56e2959c3092fc4160153703ccf5f
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2023-02-24 09:32:04 +0100

    Update: Add  exclusion

--- filesystem.sh
+++ filesystem.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Count LoC for a file or directory
 
+EXCLUDE=".clocignore"
+
 if [ $# -lt 1 ]; then
     echo "Usage: `basename $0` <fileOrDirectory>"
     exit
@@ -11,7 +13,11 @@ base="$(basename $dir)"
 
 echo "Counting $dir"
 
-cloc --force-lang-def=sonar-lang-defs.txt --report-file=$base.lang $dir 
+if [ -s $EXCLUDE ]; then
+ cloc --force-lang-def=sonar-lang-defs.txt --report-file=Report_$base.txt --exclude-dir=$(tr '\n' ',' < .clocignore) --timeout 0 $dir 
+else
+ cloc --force-lang-def=sonar-lang-defs.txt --report-file=Report_$base.txt --timeout 0 $dir 
+fi
 
 exit 0;
 
